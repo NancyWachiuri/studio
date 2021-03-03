bf6324e31416c1084e256a5c19fc524994165ca0
NancyWachiuri <nancy@nancyE6330.com>
2021-03-03 12:02:30 +0300

    Add hover effect to  js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -13,4 +13,15 @@ $(".click3"). click(function(){
   $(".show3").toggle();
   $(".hide3").toggle();
     });
-  });
+
+    $(".col-3").hover(function(){
+      $(this)("background-color", "white");
+      }, function(){
+        $('.col-3').fadeIn(500);
+      
+    });
+
+    $(".img.fluid").hover(function(){
+      $('.col-3').fadeIn(500);
+    });
+  });  
