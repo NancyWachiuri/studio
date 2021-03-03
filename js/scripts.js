d5483600f166c48a74a611345e45ef80293e82a0
NancyWachiuri <nancy@nancyE6330.com>
2021-03-03 15:47:20 +0300

    Fix identation

--- js/scripts.js
+++ js/scripts.js
@@ -3,16 +3,18 @@ $(".click1"). click(function(){
   $(".show1").toggle();
   $(".hide1").toggle();
 });
-
 $(".click2"). click(function(){
     $(".show2").toggle();
     $(".hide2").toggle();
   });
-
 $(".click3"). click(function(){
   $(".show3").toggle();
   $(".hide3").toggle();
     });
     $(".col-3").hover(function(){ 
     });
-  });
+    $("#inputName"). submit(function(event){
+alert(inputName + "we have received your message. Thank you for reaching out to us.")
+event.preventDefault();
+});
+});
